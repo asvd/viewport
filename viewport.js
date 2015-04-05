6e9dac3e39e7744dd48f5738e94371530caabae1
Dmitry Prokashev <heliosframework@gmail.com>
2015-04-05 03:15:16 +0200

    update

--- viewport.js
+++ viewport.js
@@ -18,166 +18,162 @@
 }(this,
 function (exports) {
 
-    // contains all viewports
-    var entries = [];
-    
-    
+    var entries = [];  // each entry contains a viewport with sections
+    var ctx = 50;      // context to add for the scroll targets
 
-    /**
-     * Runs through the viewports, and releases the events
-     */
-    var releaseViewports = function() {
-        for (var i = 0; i < entries.length; i++) {
-            for (var j = 0; j < entries[i].sections.length; j++) {
-                updateSection(entries[i].sections[j]);
-            }
+    // for better compression
+    var VIEWPORT = 'viewport';
+    var EventListener = 'EventListener';
+    var addEventListener = 'add'+EventListener;
+    var removeEventListener = 'remove'+EventListener;
+    var getBoundingClientRect = 'getBoundingClientRect';
 
-            entries[i].viewport.removeEventListener(
-                entries[i].viewport.vpListener
-            );
+    var top    = 'top';
+    var bottom = 'bottom';
+    var left   = 'left';
+    var right  = 'right';
+
+    var Top    = 'Top';
+    var Middle = 'Middle';
+    var Bottom = 'Bottom';
+    var Left   = 'Left';
+    var Center = 'Center';
+    var Right  = 'Right';
+
+    var Scroll = 'Scroll';
+    var Target = 'Target'
+    var ScrollTopTarget = Scroll + Top + Target;
+    var ScrollLeftTarget = Scroll + Left + Target;
+
+    var scroll = 'scroll';
+    var resize = 'resize';
+
+    var length = 'length';
+    var _window = window;
+    var _document = document;
+
+    var min = Math.min;
+
+    var reset = function() {
+        var i, j, isBody, hasViewportClass, classes,
+            listener, section, viewport, entry, sections;
+
+        // running through existing entries and removing listeners
+        for (i = 0; i < entries[length]; i++) {
+            listener = entries[i].v.vpl;
+            entries[i].v[removeEventListener](scroll, listener, 0);
+            _window[removeEventListener](resize, listener, 0);
         }
 
+        // rebuilding entries
         entries = [];
-    }
+        sections = _document.getElementsByClassName('section');
+        for (i = 0; i < sections[length]; i++) {
+            // searching for a parent viewport
+            viewport = section = sections[i];
+            do {
+                hasViewportClass = 0;
+                isBody = viewport == _document.body;
+                if (!isBody) {
+                    classes = viewport.className.split(' ');
+                    for (j = 0; j < classes[length]; j++) {
+                        if (classes[j] == VIEWPORT) {
+                            hasViewportClass = 1;
+                            break;
+                        }
+                    }
+                }
 
+                if (isBody || hasViewportClass) {
+                    viewport = viewport.scroller||viewport;
+                    break;
+                }
 
+                viewport = viewport.parentNode;
+            } while(1);
 
-    /**
-     * Collects all viewports and sections along the document, and
-     * sets the events for them
-     */
-    var setViewports = function() {
-        var sections = document.getElementsByClassName('section');
-
-        var i, j, section, listener,
-            viewport, entry,
-            len = sections.length;
-
-        // building entries
-        for (i = 0; i < len; i++) {
-            section = sections[i];
-            viewport = findViewport(section);
-
+            // searching for exisiting entry for the viewport
             entry = null;
-            for (j = 0; j < entries.length; j++) {
-                if (entries[i].viewport == viewport) {
-                    entry = entries[i];
+            for (j = 0; j < entries[length]; j++) {
+                if (entries[j].v == viewport) {
+                    entry = entries[j];
+                    break;
                 }
             }
 
             if (!entry) {
+                // creating a new entry for the viewport
                 entry = {
-                    viewport : viewport,
-                    sections : []
+                    v : viewport,
+                    s : []  // list of all sections
                 };
+
+                // listener invoked upon the viewport scroll
+                viewport.vpl = (function(entry) { return function() {
+                    var viewport = entry.v;
+
+                    // updating the data for each section
+                    for (var i = 0; i < entry.s[length]; i++) {
+                        var section = entry.s[i];
+                        
+                        var vRect = viewport[getBoundingClientRect]();
+                        var sRect = section[getBoundingClientRect]();
+                        var sHeight = sRect[bottom] - sRect[top];
+                        var sWidth  = sRect[right] - sRect[left];
+                        var vMiddle = (vRect[bottom] + vRect[top])/2;
+                        var vCenter = (vRect[left] + vRect[right])/2;
+                        var sMiddle = (sRect[bottom] + sRect[top])/2;
+                        var sCenter = (sRect[left] + sRect[right])/2;
+                        var topOffset = vRect[top] - sRect[top];
+                        var leftOffset = vRect[left] - sRect[left];
+
+                        section[VIEWPORT+Top] = topOffset / sHeight;
+
+                        section[VIEWPORT+Middle] =
+                            (vMiddle - sRect[top]) / sHeight;
+
+                        section[VIEWPORT+Bottom] =
+                            (vRect[bottom] - sRect[top]) / sHeight;
+
+                        section[VIEWPORT+Left] = leftOffset / sWidth;
+
+                        section[VIEWPORT+Center] =
+                            (vCenter - sRect[left]) / sWidth;
+
+                        section[VIEWPORT+Right] =
+                            (vRect[right] - sRect[left]) / sWidth;
+
+                        section[VIEWPORT+ScrollTopTarget] =
+                            viewport[scroll+Top] +
+                            min(sMiddle - vMiddle, -topOffset - ctx);
+
+                        section[VIEWPORT+ScrollLeftTarget] =
+                            viewport[scroll+Left] +
+                            min(sCenter - vCenter, -leftOffset - ctx);
+                    }
+                }})(entry);
+
+                viewport[addEventListener](scroll, entry.v.vpl, 0);
+                _window[addEventListener](resize, entry.v.vpl, 0);
 
                 entries.push(entry);
             }
 
-            entry.sections.push(section);
-            updateSection(section, viewport);
+            // adding section to the entry of the viewport
+            entry.s.push(section);
         }
 
-        // setting-up the events
-        for (i = 0; i < entries.length; i++) {
-            listener = entries[i].viewport.vpListener =
-                (function(sections, viewport) {
-                    return function() {
-                        for (var i = 0; i < sections.length; i++) {
-                            updateSection(sections[i], viewport);
-                        }
-                    }
-                })(entries[i].sections, entries[i].viewport);
-
-            entries[i].viewport.addEventListener(
-                'scroll', listener, 0
-            );
+        // initially setting-up the properties
+        for (i = 0; i < entries[length]; i++) {
+            entry.v.vpl();
         }
     }
 
 
-
-    /**
-     * Updates the viewport position for a single section
-     * 
-     * @param {Element} section to update data for
-     * @param {Element} viewport of an element
-     */
-    var updateSection = function(section, viewport) {
-        if (viewport) {
-            var vRect = viewport.getBoundingClientRect();
-            var sRect = section.getBoundingClientRect();
-
-            var sHeight = sRect.bottom - sRect.top;
-            var sWidth = sRect.right - sRect.left;
-
-            var vMiddle = (vRect.bottom + vRect.top) / 2;
-            var vCenter = (vRect.left + vRect.right) / 2;
-
-            section.viewportTop = (vRect.top - sRect.top) / sHeight;
-            section.viewportMiddle = (vMiddle - sRect.top) / sHeight;
-            section.viewportBottom = (vRect.bottom - sRect.top) / sHeight;
-
-            section.viewportLeft = (vRect.left - sRect.left) / sWidth;
-            section.viewportCenter = (vCenter - sRect.left) / sWidth;
-            section.viewportRight = (vRect.right - sRect.left) / sWidth;
-        } else {
-            delete section.viewportTop;
-            delete section.viewportMiddle;
-            delete section.viewportBottom;
-            delete section.viewportLeft;
-            delete section.viewportCenter;
-            delete section.viewportRight;
-        }
-    }
-
-
-
-    /**
-     * Searches for the viewport of the given section element. Runs
-     * through the parents and finds the firts element with a viewport
-     * class (if no element found, body is considered as a viewport
-     * 
-     * @param {Element} section to find viewport for
-     * 
-     * @returns {Element} viewport of the section
-     */
-    var findViewport = function(section) {
-        var viewport = section;
-
-        var i, isBody, hasViewportClass, classes;
-        do {
-            viewport = viewport.parentNode;
-            hasViewportClass = false;
-            isBody = viewport == document.body;
-
-            if (!isBody) {
-                classes = viewport.className.split(' ');
-                for (i = 0; i < classes.length; i++) {
-                    if (classes[i] == 'viewport') {
-                        hasViewportClass = true;
-                        break;
-                    }
-                }
-            }
-        } while (viewport.parentNode &&
-                 !isBody && !hasViewportClass);
-
-        return viewport;
-    }
-
-
-
-    var reset = function() {
-        releaseViewports();
-        setViewports();
-    }
-
-
-    if (document.readyState == "complete") {
+    if (_document.readyState == "complete") {
         reset();
     } else {
-        window.addEventListener("load", reset, false);
+        _window[addEventListener]("load", reset, 0);
     }
 
     exports.reset = reset;
