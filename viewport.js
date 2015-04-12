52538785795e830f814b6ed1b4f648afc8500db9
Dmitry Prokashev <heliosframework@gmail.com>
2015-04-12 04:37:54 +0200

    cleanup

--- viewport.js
+++ viewport.js
@@ -1,6 +1,6 @@
 /**
  * @fileoverview viewport - calculates viewport position
- * @version 0.0.1
+ * @version 0.0.2
  * 
  * @license MIT, see http://github.com/asvd/viewport
  * @copyright 2015 asvd <heliosframework@gmail.com> 
